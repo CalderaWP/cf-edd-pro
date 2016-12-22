7b7dc6fdc3da3a40b34c519ddbdab02af2f9c6c9
Josh Pollock <Josh@JoshPress.net>
2016-12-21 20:01:38 -0500

    inline docs in bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -5,28 +5,34 @@
  */
 add_action( 'plugins_loaded', function(){
 	include __DIR__ . '/vendor/autoload.php';
+
+	/** Load payment/bundle/pricing processors */
 	add_filter( 'caldera_forms_pre_load_processors', function() {
 		\calderawp\cfedd\cf\init\bundler::create_processor();
 		\calderawp\cfedd\cf\init\payment::create_processor();
 		\calderawp\cfedd\cf\init\pricing::create_processor();
 	});
 
+	/** Add EDD auto-population option */
 	add_action( 'caldera_forms_admin_init', function(){
 		new \calderawp\cfedd\cf\populate\admin();
 
 	});
 
+	/** Add query for EDD auto-population */
 	add_action( 'init', function(){
 		( new  \calderawp\cfedd\cf\populate\query() )->add_hooks();
 
 	});
 
+	/** Setup dynamic pricing in form */
 	add_action( 'caldera_forms_render_start', function( $form ){
 		if( is_array( $form ) ){
 			\calderawp\cfedd\cf\pricing\factory::pricing_field( $form );
 		}
 	});
 
+	/** Add REST API endpoint for dynamic pricing */
 	add_action( 'rest_api_init', function(){
 		if( ! did_action( 'caldera_forms_rest_api_init' ) ){
 			add_action( 'rest_api_init', [ 'Caldera_Forms', 'init_rest_api' ], 25 );
@@ -37,6 +43,7 @@ add_action( 'plugins_loaded', function(){
 		});
 	});
 
+	/** Setup EDD SL integration */
 	\calderawp\cfeddfields\setup::add_hooks();
 
 
